e16abb555ef7f572ba87e96164c0906304e48bb8
Belu-cat <105993877+Belu-cat@users.noreply.github.com>
2022-10-11 22:36:35 -0400

    Add files via upload

---
+++ scratchcode-to-python.py
@@ -0,0 +1,10 @@
+answer = "Nothing.."
+while not answer == "Who's there?":
+    answer = input("Knock, knock")
+
+    answer = "Nothing..."
+
+while not answer == "Mikey who":
+    answer = input("Mikey")
+
+print("Mikey dosn't work, will you let me in")
