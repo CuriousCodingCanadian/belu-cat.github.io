9ff3a3186472726678d4b627c5ee8dc1f69f26e2
CuriousCodingCanadian <74565167+CuriousCodingCanadian@users.noreply.github.com>
2023-01-09 15:00:10 -0500

    Update rock-paper-scissors.py

--- from-scratch-to-python/rock-paper-scissors.py
+++ from-scratch-to-python/rock-paper-scissors.py
@@ -3,35 +3,52 @@ from random import choice
 def youchoose():
     userin = 'Banana'
     while userin.title() not in ["Rock", "Paper", "Scissors"]:
+        if userin != 'Banana': print('Invalid Output')
         userin = input("Please enter your choice: ")
     return userin.title() 
-    
-opponentchose = "Your opponent chooses"
 
-def opponentchoose():
+def computerchoose():
     return random.choice(["Rock", "Paper", "Scissors"])
 
+def outputtie(choice):
+    print(f'Tie :| You chose {choice} and the computer chose {choice}')
+
+def outputloss(choice, cchoice):
+    print(f'You lose :( You chose {choice} and the computer chose {cchoice}')
+
+def outputwin(choice, cchoice):
+    print(f'You win :) You chose {choice} and the computer chose {cchoice}')
+    
 def main():
-    rps = ["Rock", "Paper", "Scissors"]
+    rps = ["Rock", "Paper", "Scissors", "Rock"]
     wins = 0
     losses = 0
     ties = 0
-    choice = youchoose()
-    uchoiceset = set()
+    continue = True
+    while continue:
+        choice = youchoose()
+        cchoice = computerchoose()
+        beatschoice = rps[rps.index(choice)+1]
+        if choice == cchoice:
+            outputtie(choice)
+            ties += 1
+        
+        if choice == beatscchoice:
+            outputwin(choice, cchoice)
+            wins += 1
+            
+        else: outputloss(choice, cchoice); losses += 1
     
     
+        userin = 'Banana'
+        while userin.title() not in ['True', 'False']:
+            if userin != 'Banana': print('Invalid input')
+                userin = input('Continue? (True/False)')
+                if eval(userin): continue
+                else: break
 # not complete    
-    
-    
 
-x = 0
-while x < 10:
-    choice = input("What do you want to play")
-    youchoose()
-    randchoose()
-    check()
-    x += 1
 
-# print("You lost " + str(losses) + " times.")
-# print("You won " + str(wins) + " times.")
-# print("It was a tie " + str(ties) + " times.")
+print("You lost " + str(losses) + " times.")
+print("You won " + str(wins) + " times.")
+print("It was a tie " + str(ties) + " times.")
