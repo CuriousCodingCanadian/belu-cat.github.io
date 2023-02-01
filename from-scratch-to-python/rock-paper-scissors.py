5ddfa291ad94343558c43426cb2fd91525c37df7
CuriousCodingCanadian <74565167+CuriousCodingCanadian@users.noreply.github.com>
2023-02-01 08:25:22 -0500

    Update rock-paper-scissors.py

--- from-scratch-to-python/rock-paper-scissors.py
+++ from-scratch-to-python/rock-paper-scissors.py
@@ -1,75 +1,65 @@
-from random import randint
+'''
+This is rock paper scissors. if you didnt know that, get out
 
-def youchoose():
-    youchoseappend = "You choose"
-    if choice == "rock":
-        print(youchoseappend + " Rock")
-    elif choice == "paper":
-        print(youchoseappend + " Paper")
-    elif choice == "scissors":
-        print(youchoseappend + " scissors")
+'''
 
-randchoice = randint(1,3)
-def randchoose():
-    youchoseappend = "Your opponent chooses"
-    if randchoice == 1:
-        print(youchoseappend + " Rock")
-    elif randchoice == 2:
-        print(youchoseappend + " Paper")
-    elif randchoice == 3:
-        print(youchoseappend + " scissors")
+from random import choice
 
-def check():
+def youchoose(q: bool):
+    userin = 'Banana'
+    while userin.title() not in ["Rock", "Paper", "Scissors", 'Q']:
+        if userin.title() != 'Banana': print('Invalid input')
+        if not q: userin = input("Please enter your choice: "); continue
+        else: userin = input("Please enter your choice, or Q to quit: ")
+    return userin.title() 
+
+def computerchoose():
+    return choice(["Rock", "Paper", "Scissors"])
+
+def outputtie(choice):
+    print(f'Tie :| You chose {choice} and the computer chose {choice}')
+
+def outputloss(choice, cchoice):
+    print(f'You lose :( You chose {choice} and the computer chose {cchoice}')
+
+def outputwin(choice, cchoice):
+    print(f'You win :) You chose {choice} and the computer chose {cchoice}')
+
+def results(wins, ties, losses):
+    print('|', '-'*(maxlen:=(8+(int(len(str(sorted([wins,ties,losses])[-1])))))), '|')
+    print('| Wins: ', wins,' '*(maxlen-(6+(len(str(wins))))), ' |', sep='')
+    print('| Losses: ', losses,' '*(maxlen-(8+len(str(losses)))), ' |',sep='')
+    print('| Ties: ', losses,' '*(maxlen-(6+len(str(losses)))), ' |',sep='')
+    print('|', '-'*((maxlen)), '|')
+    
+def main():
+    rps = ["Rock", "Paper", "Scissors", "Rock"]
+    rpsdict = {"Rock":"Paper", "Paper":"Scissors", "Scissors":"Rock"}
     wins = 0
     losses = 0
     ties = 0
-    if choice == "paper":
-        if randchoice == 1:
-            print("You won!")
-            wins += 1
-    
-        if randchoice == 2:
-            print("It was a tie.")
-            ties += 1
-    
-        if randchoice == 3:
-            print("Your opponent won.")
-            losses += 1
+    banana = 'banana'
+    while True:
+        choice = youchoose(banana != 'banana')
+        cchoice = computerchoose()
+        beatscchoice = rpsdict[cchoice]
 
-    if choice == "rock":
-        if randchoice == 1:
-            print("It was a tie.")
-            ties += 1
-    
-        if randchoice == 2:
-            print("Your opponent won.")
-            losses += 1
-    
-        if randchoice == 3:
-            print("You won!")
-            wins += 1
+        if choice == 'Q': break
+        
+        if choice == cchoice: outputtie(choice); ties += 1
+        
+        elif choice == beatscchoice: outputwin(choice, cchoice); wins += 1
+            
+        else: outputloss(choice, cchoice); losses += 1
 
-    if choice == "scissors":
-        if randchoice == 1:
-            print("Your opponent won.")
-            losses += 1
+        banana = 'apple'
     
-        if randchoice == 2:
-            print("You won!")
-            wins += 1
-    
-        if randchoice == 3:
-            print("It was a tie.")
-            ties += 1
 
-x = 0
-while x < 10:
-    choice = input("What do you want to play")
-    youchoose()
-    randchoose()
-    check()
-    x += 1
 
-# print("You lost " + str(losses) + " times.")
-# print("You won " + str(wins) + " times.")
-# print("It was a tie " + str(ties) + " times.")
+    results(wins, ties, losses)
+    #print("You lost " + str(losses) + " times.")
+    #print("You won " + str(wins) + " times.")
+    #print("It was a tie " + str(ties) + " times.")
+
+if __name__ == '__main__':
+    main()
